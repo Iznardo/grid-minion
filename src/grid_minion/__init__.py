91695dbc3564859c654e0097f3bad29bddec7088
F1RE <contact@f1re.es>
2025-12-29 11:52:53 +0100

    Creación de una herramienta para separar los archivos de GRID en las diferentes partidas

--- src/grid_minion/__init__.py
+++ src/grid_minion/__init__.py
@@ -1,4 +1,5 @@
 from .graphql_client import GridGraphQLClient
 from .rest_client import GridRestClient
+from .utils import split_grid_series
 
-__all__ = ["GridGraphQLClient", "GridRestClient"]
+__all__ = ["GridGraphQLClient", "GridRestClient", "split_grid_series"]
